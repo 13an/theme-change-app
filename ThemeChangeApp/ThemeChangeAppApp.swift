9524820a73dfa8e17aa19239fb372eb905b17451
13an <banjoyatakumi@gmail.com>
2023-07-18 16:47:38 +0900

    initial commit

---
+++ ThemeChangeApp/ThemeChangeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeChangeAppApp.swift
+//  ThemeChangeApp
+//
+//  Created by 13an on 2023/07/18.
+//
+
+import SwiftUI
+
+@main
+struct ThemeChangeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
